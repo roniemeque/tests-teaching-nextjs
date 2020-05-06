69c87b3c2ee3f03a0e977379a9359469a3357ee3
Ronie <roniemeque@icloud.com>
2020-05-06 00:12:53 -0300

    adding nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,15 @@ import { Global } from "@emotion/core";
 import styled from "../styles/styled";
 import Nav from "../components/app/Nav";
 import Footer from "../components/app/Footer";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
