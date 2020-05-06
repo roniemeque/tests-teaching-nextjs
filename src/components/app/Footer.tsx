4f0f2260bfb85a1cc13c6164496db0a7cd1bbabc
Ronie <roniemeque@icloud.com>
2020-05-05 22:29:09 -0300

    footer component

---
+++ src/components/app/Footer.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import styled from "../../styles/styled";
+
+const Footer: FC = () => {
+  return <FooterStyled>footer</FooterStyled>;
+};
+
+const FooterStyled = styled.footer``;
+
+export default Footer;
